1cc6725fe314ac507a7ae05d6323ad8ce8794f4c
j-forster <25847959+j-forster@users.noreply.github.com>
2020-12-14 12:51:14 +0100

    Refactor

--- examples/lorawan/simple/simple.ino
+++ examples/lorawan/simple/simple.ino
@@ -1,9 +1,5 @@
 
-#include <SPI.h>
-#include "SX1272.h"
-#include "local_lorawan.h"
-#include "WaziDev.h"
-#include <CayenneLPP.h>
+#include <WaziDev.h>
 
 unsigned char LoRaWANKey[16] = {0x23, 0x15, 0x8D, 0x3B, 0xBC, 0x31, 0xE6, 0xAF, 0x67, 0x0D, 0x19, 0x5B, 0x5A, 0xED, 0x55, 0x25};
 unsigned char DevAddr[4] = {0x26, 0x01, 0x1D, 0x87};
@@ -49,7 +45,7 @@ void loop(void)
   serialPrintf("LoRa receive ... ");
   uint8_t offs = 0;
   long startSend = millis();
-  e = wazidev.receiveLoRaWAN(payload, &offs, &len, 10000);
+  e = wazidev.receiveLoRaWAN(payload, &offs, &len, 6000);
   long endSend = millis();
   if (e != 0)
   {
